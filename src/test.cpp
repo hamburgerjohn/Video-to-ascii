883b4041914a7bee7424dd0c5ff9e46c21a50163
hamburgerjohn <joebob2154@gmail.com>
2022-04-12 16:19:43 -0400

    reconstruct video file

--- src/test.cpp
+++ src/test.cpp
@@ -13,13 +13,13 @@ int main(int argc, char const **argv)
     VideoCapture capture(std::string(path) + "/" + std::string(argv[1])); // open video source
 
     system(std::string("mkdir " + std::string(path) + "/../frames").c_str());
-    system(std::string("rm " + std::string(path) + "/../frames/*.png").c_str());
     
     if(!capture.isOpened()){
         std::cout << "error opening stream\n";
         return -1;
     }
 
+    //extract frames
     for(int i = 0; i < capture.get(CAP_PROP_FRAME_COUNT); i++){
         capture.set(CAP_PROP_POS_FRAMES, i);
         Mat frame;
@@ -28,9 +28,14 @@ int main(int argc, char const **argv)
             std::cout << "Failed to extract a frame.\n";
             return -1;
         }
-
-        imwrite("../frames/" + std::to_string(i) + ".png", frame);
+        
+        imwrite("../frames/img" + std::to_string(i) + std::string(".png"), frame);
     }
+
+    //pack images into mp4
+    system("ffmpeg -start_number 0 -i ../frames/img%d.png -vcodec mpeg4 -s 1920x1080 test.avi");
+    system(std::string("rm " + std::string(path) + "/../frames/*.png").c_str());
+
 
     return 0;
 }
